a7bdfae0cab547ac58e3d020a1bdbbd9aa5a0a6d
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-09-12 14:08:36 +0400

    Added test for wait and notify methods

---
+++ src/TestOfPatternProducerConsumer/TestWaitNotify.java
@@ -0,0 +1,62 @@
+package TestOfPatternProducerConsumer;
+
+import ConsoleColors.ConsoleColors;
+
+import java.util.Scanner;
+
+public class TestWaitNotify {
+    public static void main(String[] args) throws InterruptedException {
+        WaitAndNotify wn = new WaitAndNotify();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wn.produce();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wn.consume();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
+
+class WaitAndNotify {
+    public void produce() throws InterruptedException {
+        synchronized (this) {
+            System.out.println("Producer thread started...");
+            wait();
+            System.out.println("Producer thread resumed...");
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        Thread.sleep(2000);
+        Scanner scanner = new Scanner(System.in);
+
+        synchronized (this) {
+            System.out.println(ConsoleColors.BLUE + "Waiting for return press key...");
+            scanner.nextLine();
+            notify();
+            System.out.println("Wait...");
+            Thread.sleep(5000);
+            System.out.println("Ok..." + ConsoleColors.RESET);
+        }
+    }
+}
